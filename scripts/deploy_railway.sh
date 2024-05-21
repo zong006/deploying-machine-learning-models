8c9b0334142600c6e864efd38df2686b3a441b78
zong006 <ongzhicong@gmail.com>
2024-05-21 22:54:04 +0800

    resolve deploy railway issue login railway 7

--- scripts/deploy_railway.sh
+++ scripts/deploy_railway.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 cd section-07-ci-and-publishing/house-prices-api
-echo ${RAILWAY_TOKEN}
+echo ${MY_ENV_VAR}
 # 5ad7efcd-994e-4a79-bfb1-7af273b19170
 # RAILWAY_TOKEN=${RAILWAY_TOKEN} railway run link
 # Run railway link
-expect -c '
-spawn RAILWAY_TOKEN=${RAILWAY_TOKEN} railway link
-expect "? Select a project"
-send "house_api\r"  
-expect "? Select an environment"
-send "production\r"
-'
+# expect -c '
+# spawn RAILWAY_TOKEN=${RAILWAY_TOKEN} railway link
+# expect "? Select a project"
+# send "house_api\r"  
+# expect "? Select an environment"
+# send "production\r"
+# '
 
 
-# Run railway up --detach
-railway up --detach
+# # Run railway up --detach
+# railway up --detach
