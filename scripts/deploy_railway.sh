ab067657c04c145edfa7dd27fd6b83fe1e0fe199
zong006 <ongzhicong@gmail.com>
2024-05-21 21:29:02 +0800

    resolve deploy railway issue

--- scripts/deploy_railway.sh
+++ scripts/deploy_railway.sh
@@ -1,33 +1,16 @@
 #!/bin/bash
 
-# Install expect utility
-sudo apt-get update && sudo apt-get install -y expect
-
 cd section-07-ci-and-publishing/house-prices-api
-
-# Authenticate with Railway using the token
-railway login --token "$RAILWAY_TOKEN"
-
-# Run railway init
-expect -c '
-spawn railway init
-expect "? Project Name "
-send "house\r"  
-expect {
-    "Leave blank to generate a random name" {
-        send "\r"
-    }
-}
-expect eof
-'
 
 # Run railway link
 expect -c '
 spawn railway link
 expect "? Select a project"
 send "house\r"  
-expect eof
+expect "? Select an environment"
+send "production\r"
 '
+
 
 # Run railway up --detach
 railway up --detach
